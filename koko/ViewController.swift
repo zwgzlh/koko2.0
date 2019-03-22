69ffae22af6ff1f515dfee9a862e2d475bd4f010
Lisa <lisasmolentseva@macbook-air-lisa.local>
2019-03-22 17:24:31 +0300

    Initial Commit

---
+++ koko/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  koko
+//
+//  Created by Lisa Smolentseva on 22/03/2019.
+//  Copyright © 2019 Lisa Smolentseva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
